a1dccc711f88880332fd67ba42078ea877496da9
Pratham Rathore <prathampipliya1234@gmail.com>
2026-08-09 15:25:13 +0530

    Updated image

--- aaroh_frontend/src/components/hero/Hero.jsx
+++ aaroh_frontend/src/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { ArrowRight, Sparkles, Users, FolderKanban } from "lucide-react";
 import { Link } from "react-router-dom";
+import aarohLogo from "../../assets/images/logo.png";
 
 function Hero() {
   return (
@@ -78,7 +79,7 @@ function Hero() {
 
             <div className="hero-card-content">
               {/* <span>INNOVATION ECOSYSTEM</span> */}
-              <strong>आROH</strong>
+              <img src={aarohLogo} alt="AAROH Logo" className="hero-logo-image" />
               <p>Connecting Student Innovation</p>
             </div>
           </motion.div>
